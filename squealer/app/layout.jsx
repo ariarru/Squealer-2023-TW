a4cb10a0f05bd4540acc485ef8dbf37615a09582
Samoggino <simone.samoggia2@studio.unibo.it>
2023-09-19 18:47:26 +0200

    Fix layout
    Problema legato a tag html e body

--- squealer/app/layout.jsx
+++ squealer/app/layout.jsx
@@ -5,19 +5,23 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Squealer',
-  description:
-    'La nuova versione di Squealer, il social network per eccellenza.'
+  description: 'La nuova versione di Squealer, il social network per eccellenza.'
 }
-export default function RootLayout ({ children }) {
+
+export default function RootLayout({ children }) {
   return (
-    <div className='md:flex mt-4 max-w-4xl mx-auto gap-6 '>
-      <div
-        className={
-          'mx-2 relative top-36 md:top-0  md:mx-0 md:w-9/12 md:left-1/4'
-        }
-      >
-        {children}
-      </div>
-    </div>
+    <html lang='en'>
+      <body className={inter.className}>
+
+        <div className='md:flex mt-4 max-w-4xl mx-auto gap-6 '>
+          <div className={'mx-2 relative top-36 md:top-0  md:mx-0 md:w-9/12 md:left-1/4'}>
+
+            {children}
+
+          </div>
+        </div>
+
+      </body>
+    </html>
   )
 }
